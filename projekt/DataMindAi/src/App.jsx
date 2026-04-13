0a0e6379c59c1f6b54bfb28c05ef99e0984edc3c
Filip <filipstrzalkaa@gmail.com>
2026-04-13 10:37:45 +0200

    poprawka sidebara znowu

--- projekt/DataMindAi/src/App.jsx
+++ projekt/DataMindAi/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from './Pages/LoginPage'
 import RegisterPage from './Pages/RegisterPage'
 import DashboardPage from './Pages/DashboardPage'
 import LecturesPage from './Pages/LecturesPage'
+import AIChatPage from './Pages/AIChatPage'
 import UserSettingsPage from './Pages/UserSettingsPage'
 import NotFoundPage from './Pages/NotFoundPage'
 
@@ -17,6 +18,8 @@ function App() {
         <Route path="/rejestracja" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/lekcje" element={<LecturesPage />} />
+        <Route path="/ai-chat" element={<AIChatPage />} />
+        <Route path="/osiagniecia" element={<UserSettingsPage />} />
         <Route path="/ustawienia" element={<UserSettingsPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
--- projekt/DataMindAi/src/Pages/LecturesPage.jsx
+++ projekt/DataMindAi/src/Pages/LecturesPage.jsx
@@ -1,39 +1,16 @@
 import './LecturesPage.css'
-import { Link } from 'react-router-dom'
-import Lectures from '../Components/Lectures'
 import { useState } from 'react'
 import SidebarHeader from '../Components/SidebarHeader'
+import Lectures from '../Components/Lectures'
 
 function LecturesPage() {
-  
   const [sidebarOpen, setSidebarOpen] = useState(true)
 
-    return(
-        <div style={{ position: 'relative' }}>
-           {sidebarOpen && (
-    <button 
-        onClick={() => setSidebarOpen(false)}
-        style={{
-            position: 'fixed',
-            top: '23px',
-            left: '155px',
-            zIndex: 200,
-            background: 'none',
-            border: 'none',
-            color: 'white',
-            cursor: 'pointer',
-            fontSize: '1.1rem',
-            display: window.innerWidth <= 640 ? 'block' : 'none'
-        }}
-    >
-        ✕
-    </button>
-)}
-            <SidebarHeader sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}>
-                <Lectures />
-            </SidebarHeader>
-        </div>
-    )
+  return (
+    <SidebarHeader sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}>
+      <Lectures />
+    </SidebarHeader>
+  )
 }
 
 export default LecturesPage

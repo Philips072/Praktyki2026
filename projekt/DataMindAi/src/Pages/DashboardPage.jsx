3bd873982f1c9703ce1d8aa4fdab90047c422cf3
Filip <filipstrzalkaa@gmail.com>
2026-04-13 10:01:04 +0200

    poprawka sidebara

--- projekt/DataMindAi/src/Pages/DashboardPage.jsx
+++ projekt/DataMindAi/src/Pages/DashboardPage.jsx
@@ -8,31 +8,10 @@ function DashboardPage(){
     const [sidebarOpen, setSidebarOpen] = useState(true)
 
     return(
-        <div style={{ position: 'relative' }}>
-           {sidebarOpen && (
-    <button 
-        onClick={() => setSidebarOpen(false)}
-        style={{
-            position: 'fixed',
-            top: '23px',
-            left: '155px',
-            zIndex: 200,
-            background: 'none',
-            border: 'none',
-            color: 'white',
-            cursor: 'pointer',
-            fontSize: '1.1rem',
-            display: window.innerWidth <= 640 ? 'block' : 'none'
-        }}
-    >
-        ✕
-    </button>
-)}
-            <SidebarHeader sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}>
-                <Dashboard1 />
-                <Dashboard2 />
-            </SidebarHeader>
-        </div>
+        <SidebarHeader sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}>
+            <Dashboard1 />
+            <Dashboard2 />
+        </SidebarHeader>
     )
 }
 
